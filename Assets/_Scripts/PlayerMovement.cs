67dc31d7fa1af52967c200f84aede939c042c8d2
EmilTheApril <pjuske.2002@hotmail.com>
2023-04-15 13:17:56 +0200

    Movement, no mouse

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
+    private bool jumping;
+    private bool isGrounded;
+
+    private Vector3 dir;
 
     private Rigidbody rb;
 
@@ -17,13 +22,33 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+        Jump();
+    }
+
+    private void OnMovement(InputValue inputValue)
+    {
+        dir = new Vector3(inputValue.Get<Vector2>().x, rb.velocity.y, inputValue.Get<Vector2>().y);
+    }
+
+    private void OnJump(InputValue inputValue)
+    {
+        if (inputValue.Get<float>() == 1) jumping = true; else jumping = false;
     }
 
     private void Move()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.velocity = transform.forward * speed;
-        }
+        rb.velocity = new Vector3(dir.x * speed, rb.velocity.y, dir.z * speed);
+    }
+
+    private void Jump()
+    {
+        if (!jumping || !isGrounded) return;
+        isGrounded = false;
+        rb.AddForce(transform.up * jumpForce);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        isGrounded = true;
     }
 }
